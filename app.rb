977e5ab1235fbbfc3eeb61059e9e930b511bf9d1
Hanna Lee <mani21na@gmail.com>
2019-11-03 22:52:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :session
+    set :session_secret, "secret"
+  end 
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkput' do
+    session["item"] = params[:itme]
+    @session = session
+  
 end
